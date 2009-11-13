637bc428e4a325b8eeb565769e78813aa6cdfe9a
Evan K. Friis <ekfriis@gmail.com>
2009-11-12 16:28:24 -0800

    First commit of graph nodes

--- ants/graph.py
+++ ants/graph.py
@@ -1,10 +1,21 @@
 import datetime as dt
+import numpy as np
+from ants.geocoders import gmaps as metric
 
-class Vertex(object):
-   def __init__(location=None, time_windows=None, service_time=None):
-      self.location = location
+class Destination(object):
+   ''' A delivery destination '''
+   def __init__(address=None, time_pref=None, delivery_time_avg=0, delivery_time_error=0):
+      self.address = address
+      self.time_pref = time_pref
+      self.lat, self.lng = metric.lat_lng(address)
 
-   def 
+   def distance_to(self, other):
+      return metric.distance_between(self.address, other.address)
+
+   def time_to(self, other):
+      return metric.time_between(self.address, other.address)
+
+   def throw_delivery_time(self):
+      return numpy.random
 
 
-class Graph
