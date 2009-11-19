f4fc40e2b0b71a0146b29d1d97d0071a0078decc
Evan K. Friis <ekfriis@gmail.com>
2009-11-19 10:50:21 -0800

    Docstrings + pep8

--- ants/geocoders/gmaps.py
+++ ants/geocoders/gmaps.py
@@ -1,47 +1,58 @@
-from googlemaps import GoogleMaps
+''' ants.geocoders.gmaps
 
+Infromterface to the google maps api.  Provides latitude/longitude lookups from
+address, and driving time/distance for address pairs.
+
+'''
+
+from googlemaps import GoogleMaps, GoogleMapsError
 from ants.geocoders.gmaps_api_key import api_key
 
-_lat_lng_cache = {}
-_directions_cache = {}
+_LAT_LNG_CACHE = {}
+_DIRECTIONS_CACHE = {}
 
-_gmaps = GoogleMaps(api_key)
+_GMAPS = GoogleMaps(api_key)
 
 class MetricError(ValueError):
-   pass
+    ''' Exception raised when metric fails '''
+    pass
 
 def clear():
-   _lat_lng_cache = {}
-   _directions_cache = {}
+    ''' Clear all metric lookup caches '''
+    _LAT_LNG_CACHE.clear()
+    _DIRECTIONS_CACHE.clear()
 
 def lat_lng(address):
-   ''' Get latitude/long from an address'''
-   if address not in _lat_lng_cache:
-      _lat_lng_cache[address] = _gmaps.address_to_latlng(address)
-   return _lat_lng_cache[address]
+    ''' Get latitude/long from an address'''
+    if address not in _LAT_LNG_CACHE:
+        _LAT_LNG_CACHE[address] = _GMAPS.address_to_latlng(address)
+    return _LAT_LNG_CACHE[address]
 
 def time_between(address_1, address_2):
-   ''' Return the traveling time between two addresses in minutes '''
-   # Time between the same place is always zero
-   if address_1 == address_2:
-      return 0
-   return directions(address_1, address_2)['Directions']['Duration']['seconds']/60.
+    ''' Return the traveling time between two addresses in minutes '''
+    # Time between the same place is always zero
+    if address_1 == address_2:
+        return 0
+    return directions(address_1, address_2)['Directions']\
+        ['Duration']['seconds']/60.
 
 def distance_between(address_1, address_2):
-   ''' Return the distance between two addresses in meters '''
-   # Dist between the same place is always zero
-   if address_1 == address_2:
-      return 0
-   return directions(address_1, address_2)['Directions']['Distance']['meters']
+    ''' Return the distance between two addresses in meters '''
+    # Dist between the same place is always zero
+    if address_1 == address_2:
+        return 0
+    return directions(address_1, address_2)['Directions']\
+        ['Distance']['meters']
 
 def directions(address_1, address_2):
-   ''' Get directions from Google '''
-   # Look it up on google if we haven't already
-   address_pair = (address_1, address_2)
-   if address_pair not in _directions_cache:
-      try:
-         _directions_cache[address_pair] = _gmaps.directions(address_1, address_2)
-      except GoogleMapsError:
-         raise MetricError, " google maps can't find address!"
-   return _directions_cache[address_pair]
+    ''' Get directions from Google '''
+    # Look it up on google if we haven't already
+    address_pair = (address_1, address_2)
+    if address_pair not in _DIRECTIONS_CACHE:
+        try:
+            _DIRECTIONS_CACHE[address_pair] = ( 
+                _GMAPS.directions(address_1, address_2))
+        except GoogleMapsError:
+            raise MetricError, " google maps can't find address!"
+    return _DIRECTIONS_CACHE[address_pair]
 
