cd827d5b63c6eefd9f0fc847376ef04742d1cd75
Evan K. Friis <ekfriis@gmail.com>
2009-11-19 10:29:43 -0800

    Adding documentation, now clean by Pylint + PEP8

--- ants/graph.py
+++ ants/graph.py
@@ -1,45 +1,89 @@
-import datetime as dt
+''' Ants.Graph
+
+Provides a graph node (Destination) and various utilities to construct the
+edges (distances between destinations)
+
+'''
+
 from ants.geocoders import gmaps as metric
 import numpy as np
 
 class Destination(object):
-    ''' A delivery destination '''
-    def __init__(self, address=None, time_pref=None, delivery_time_avg=4, delivery_time_variance=2):
+    ''' A delivery destination
+
+    Stores an address, the latitude and longitude, and the time preferences for
+    this destination.  Contains functions to determine times/distances to other
+    destinations and a throw_delivery_time() function that can simulate variable
+    delivery durations at this address.
+    
+    '''
+    def __init__(self, address=None, time_pref=None, 
+                 delivery_time_avg=4, delivery_time_variance=2):
         self.address = address
         self.time_pref = time_pref
         self.lat, self.lng = metric.lat_lng(address)
-        # Compute our statistical distribution to use to throw
-        #  expected delivery times.  Use a gamma function - for gamma,
-        #  with shape parameter k and scale parameter theta, 
-        #  mean = k theta; variance = k theta^2
-        #  therefore, theta = variance/mean, k = mean^2/variance
+
         scale = float(delivery_time_variance)/delivery_time_avg
-        shape = float(delivery_time_avg*delivery_time_avg)/delivery_time_variance
+        shape = float(delivery_time_avg*delivery_time_avg) / \
+                delivery_time_variance
+
         def gamma_func():
+            ''' Destination delivery time random throw 
+
+                 Compute our statistical distribution to use to throw expected
+                 delivery times.  Use a gamma function - for gamma, with shape
+                 parameter k and scale parameter theta, mean = k theta;
+                 variance = k theta^2 therefore, theta = variance/mean, k =
+                 mean^2/variance
+
+            '''
             return np.random.gamma(shape, scale)
+
         self.random_delivery_time = gamma_func
 
     def throw_delivery_time(self):
-       return self.random_delivery_time()
+        ''' Return a random value representing delivery duration '''
+        return self.random_delivery_time()
 
     def distance_to(self, other):
-       return metric.distance_between(self.address, other.address)
+        ''' Return distance (in meters) to another Destination '''
+        return metric.distance_between(self.address, other.address)
 
     def time_to(self, other):
-       return metric.time_between(self.address, other.address)
+        ''' Return time (in minutes) to another Destination '''
+        return metric.time_between(self.address, other.address)
 
-    def compatability_to(self, other, n=500):
-       ''' Determine schedule compatability to another destination '''
-       transit_time = self.time_to(other)
+    def compatability_to(self, other, iterations=500):
+        ''' Determine schedule compatability to another destination 
 
-       satisfaction_prob = 0.
+            Compatability is defined as the average satisfaction probability of
+            the other destination, taking into account the delivery time here
+            (variable) and transit time (should be variable) supposing that the
+            arrival time at *this* destination is distributed according to
+            *this* destination's time preferences.
 
-       for i in range(n):
+        '''
+        # Get time to other destination - eventually this should be Monte
+        # Carlo'd
+        transit_time = self.time_to(other)
+        satisfaction_prob = 0.
+
+        count = 0
+        while count < iterations:
+            count += 1
+            # Throw a bunch of random arrival times, assuming a priori that the
+            # time this location recieves it's package is distributed according
+            # to its time preference distribution.
             arrival_time_here = self.time_pref.random()
             delivery_time_here = self.throw_delivery_time()
 
-       arrival_time_there = arrival_time_here + delivery_time_here + transit_time
-       satisfaction_prob += other.satisfaction_probability(arrival_time_there)
+            # Arrival time there is deterministic
+            arrival_time_there = arrival_time_here + \
+                    delivery_time_here + transit_time
+            # Get the probability of satisfaction for the other Destination for
+            # this arrial time
+            satisfaction_prob += \
+                    other.satisfaction_probability(arrival_time_there)
 
-       return satisfaction_prob/n
+        return satisfaction_prob/iterations
 
