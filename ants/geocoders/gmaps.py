0961ffc081a5cced8885d0268f3a00e1e9ddbf62
Evan K. Friis <ekfriis@gmail.com>
2009-12-16 10:52:04 -0800

    Add verbosity option

--- ants/geocoders/gmaps.py
+++ ants/geocoders/gmaps.py
@@ -15,9 +15,28 @@ _DIRECTIONS_CACHE = {}
 
 _GMAPS = GoogleMaps(api_key)
 
+# Verbosity
+class Verbosity(object):
+    def __init__(self):
+        self.loud = False
+    def set_loud(self, isLoud):
+        self.loud = isLoud
+    def is_loud(self):
+        return self.loud
+
+_VERBOSITY = Verbosity()
+
 class MetricError(ValueError):
     ''' Exception raised when metric fails '''
     pass
+
+def loud():
+    ''' Print address as they are loaded '''
+    _VERBOSITY.set_loud(True)
+
+def quiet():
+    ''' Suppress stdout '''
+    _VERBOSITY.set_loud(False)
 
 def clear():
     ''' Clear all metric lookup caches '''
@@ -25,25 +44,31 @@ def clear():
     _DIRECTIONS_CACHE.clear()
 
 def save(filename='gmaps.gis'):
+    ''' Save cached address info to file '''
     output = file(filename, 'wb')
     pickle.dump(_LAT_LNG_CACHE, output)
     pickle.dump(_DIRECTIONS_CACHE, output)
     output.close()
 
 def load(filename='gmaps.gis'):
+    ''' Load cached address info from file '''
     if os.path.exists(filename):
-        input = file(filename, 'rb')
+        input_file = file(filename, 'rb')
         clear()
-        _LAT_LNG_CACHE.update(pickle.load(input))
-        _DIRECTIONS_CACHE.update(pickle.load(input))
+        _LAT_LNG_CACHE.update(pickle.load(input_file))
+        _DIRECTIONS_CACHE.update(pickle.load(input_file))
     else:
         print "Error: could not load metric file: %s" % filename
 
 def lat_lng(address):
     ''' Get latitude/long from an address'''
+    if _VERBOSITY.is_loud():
+        print "Lat/Lng:", address
+
     if address not in _LAT_LNG_CACHE:
         try:
-            _LAT_LNG_CACHE[address] = _GMAPS.address_to_latlng(address)
+            result = _GMAPS.address_to_latlng(address)
+            _LAT_LNG_CACHE[address] = result
         except GoogleMapsError:
             raise MetricError, " google maps can't find address:", address
     return _LAT_LNG_CACHE[address]
@@ -66,17 +91,20 @@ def distance_between(address_1, address_2):
 
 def directions(address_1, address_2):
     ''' Get directions from Google '''
+    if _VERBOSITY.is_loud():
+        print "Dir: %s ==> %s" % (address_1, address_2)
     # Look it up on google if we haven't already
     address_pair = (address_1, address_2)
-    # Validate addresses and put lat/lng in cache
-    lat_lng(address_1)
-    lat_lng(address_2)
     if address_pair not in _DIRECTIONS_CACHE:
+        # Validate addresses and put lat/lng in cache
+        lat_lng(address_1)
+        lat_lng(address_2)
         try:
-            _DIRECTIONS_CACHE[address_pair] = ( 
-                _GMAPS.directions(address_1, address_2))
+            result = _GMAPS.directions(address_1, address_2)
+            _DIRECTIONS_CACHE[address_pair] = result
         except GoogleMapsError:
             raise MetricError, \
-                    " google maps can't get directions for %s-%s"%(address_1, address_2)
+                    " google maps can't get directions for %s-%s" \
+                    % (address_1, address_2)
     return _DIRECTIONS_CACHE[address_pair]
 
