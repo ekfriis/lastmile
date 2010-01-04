deb4cdb528364fe61fab157248ff0d034e88c6ad
Evan K. Friis <ekfriis@gmail.com>
2010-01-03 18:39:39 -0800

    Adding rootfinder tests

--- ants/run_tests.py
+++ ants/run_tests.py
@@ -8,6 +8,7 @@ from ants.tests.engine_timing import *
 from ants.tests.operations import *
 from ants.tests.parameters import *
 from ants.tests.routemap import *
+from ants.tests.rootfinder import *
 
 if __name__ == "__main__":
    unittest.main()
