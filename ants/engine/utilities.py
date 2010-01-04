07ec5f4cf79754fe56d9c880ce208b1783803cc7
Evan K. Friis <ekfriis@gmail.com>
2010-01-03 18:39:20 -0800

    Add fix to consecutive pairs for generator input

--- ants/engine/utilities.py
+++ ants/engine/utilities.py
@@ -4,14 +4,16 @@ Utility functions used by the solver.
 Author: Evan K. Friis
 '''
 
-from itertools import izip, islice
+from itertools import izip, islice, tee
 
 def consecutive_pairs(the_list):
-    ''' Generator which returns consecutive pairs from an iterable 
+    ''' Generator which returns consecutive pairs from an iterable/generator
     
     Example:
     >>> list(consecutive_pairs([1,2,3,4]))
     >>> [(1,2), (2,3), (3,4)]
     '''
-    for first, second in izip(the_list, islice(the_list, 1, None)):
+    # Get independent generators
+    copy_1, copy_2 = tee(the_list, 2)
+    for first, second in izip(copy_1, islice(copy_2, 1, None)):
         yield first, second
